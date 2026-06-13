edebf4df97a8748acdf3e507eaa2ac7a84cac53e
ayushbajpai373-coder <ayushbajpai373@gmail.com>
2026-06-13 22:54:32 +0530

    add secret key fix login

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = "your-secret-key"
+    SECRET_KEY = '7f9K2xPq8LmZ91Ab'
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
